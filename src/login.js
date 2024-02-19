a419688320755245602e7c0f0821b6f93481a22d
NjorogeMaurice <njoroge0330@gmail.com>
2024-02-19 08:41:32 +0300

    testing integration

--- src/login.js
+++ src/login.js
@@ -36,7 +36,7 @@ export const Login = () => {
       
       <div className="flex justify-center items-center h-screen">
   <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={submit}>
-    <h3 className="text-lg font-bold mb-6">Welcome Back!</h3>
+    <h3 className="text-lg font-bold mb-6">Welcome Back!!</h3>
     <div className="mb-4">
       <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">Email</label>
       <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Enter E-mail" name='email' value={username} required onChange={e => setUsername(e.target.value)} />
